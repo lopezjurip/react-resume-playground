cf2f0dfdec4fc65148528329bb1c60f758b1a72a
Patricio López <lopezjuripatricio@gmail.com>
2017-04-09 00:57:08 -0300

    New theme

--- src/App.js
+++ src/App.js
@@ -6,19 +6,19 @@ import Resume, {
   Section,
 } from 'react-resume';
 import Github from "react-resume-github";
-import theme from 'react-resume-theme-slick';
+import * as themes from 'react-resume-themes';
 
 class App extends Component {
   render() {
     return (
-      <Resume theme={theme()}>
+      <Resume theme={themes.bauer()}>
         <Basics>
           <Basics.Name>Patricio López J.</Basics.Name>
           <Basics.Label>Software Engineer.</Basics.Label>
           <Basics.Location>Av. Portugal 442, Santiago</Basics.Location>
-          <Basics.Email type="email">patricio@lopezjuri.com</Basics.Email>
-          <Basics.Website type="website">lopezjuri.com</Basics.Website>
-          <Basics.Phone type="phone">+56 9 90511003</Basics.Phone>
+          <Basics.Email>patricio@lopezjuri.com</Basics.Email>
+          <Basics.Website>lopezjuri.com</Basics.Website>
+          <Basics.Phone>+56 9 90511003</Basics.Phone>
           <Basics.Summary>
             Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
           </Basics.Summary>
