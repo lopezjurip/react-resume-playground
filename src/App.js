f5d87f676be0892bc4aeef58aeea9e4e886480f9
Patricio López <lopezjuripatricio@gmail.com>
2017-04-09 06:06:51 -0300

    Fix profile social

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Resume, {
   Basics,
   Work,
   Volunteer,
+  References,
   Section,
 } from 'react-resume';
 import Github from "react-resume-github";
@@ -15,39 +16,39 @@ class App extends Component {
         <Basics>
           <Basics.Name>Patricio López J.</Basics.Name>
           <Basics.Label>Software Engineer.</Basics.Label>
-          <Basics.Location>Av. Portugal 442, Santiago</Basics.Location>
+          <Basics.Location>Av. Vicuña Mackenna 6100, Santiago</Basics.Location>
           <Basics.Email>patricio@lopezjuri.com</Basics.Email>
           <Basics.Website>lopezjuri.com</Basics.Website>
-          <Basics.Phone>+56 9 90511003</Basics.Phone>
+          <Basics.Phone>+56 9 87654321</Basics.Phone>
           <Basics.Summary>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            Rocket scientist, Software drawer, IT consultant and personal psychologist. Interested in state of the art technology and time-bomb hacks.
           </Basics.Summary>
           <Basics.Profiles>
-            <Basics.Profile network="facebook" username="mrpatiwi" />
-            <Basics.Profile network="github" username="mrpatiwi" />
+            <Basics.Profile network="twitter" username="mrpatiwi" />
+          <Basics.Profile network="linkedin" username="mrpatiwi" />
           </Basics.Profiles>
         </Basics>
         <Work>
           <Work.Title>Work Experience</Work.Title>
           <Work.Items>
             <Work.Item>
-              <Work.Item.Company>Alma Máter UC</Work.Item.Company>
-              <Work.Item.Position>Founder</Work.Item.Position>
-              <Work.Item.Website href="https://lopezjuri.com">Website</Work.Item.Website>
-              <Work.Item.Period start="01-01-2016" />
+              <Work.Item.Company>Mediastream</Work.Item.Company>
+              <Work.Item.Position>Software Engineer</Work.Item.Position>
+              <Work.Item.Website href="https://mediastre.am">Website</Work.Item.Website>
+              <Work.Item.Period start="2016-12-01" end="2017-3-01" />
               <Work.Item.Summary>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                Full-time summer job developing Node.js and React and React-Native applications.
               </Work.Item.Summary>
-              <Work.Item.Highlights>
-                <Work.Item.Highlight>Started</Work.Item.Highlight>
-                <Work.Item.Highlight>Finished</Work.Item.Highlight>
+              <Work.Item.Highlights bullets>
+                <Work.Item.Highlight>Requirement gathering</Work.Item.Highlight>
+                <Work.Item.Highlight>Design and setup with React and Redux</Work.Item.Highlight>
               </Work.Item.Highlights>
             </Work.Item>
           </Work.Items>
         </Work>
         <Section>
           <Section.Title>Experience</Section.Title>
-          <Github username="mrpatiwi">
+          <Github username="mrpatiwi" host="https://78f34fc9.ngrok.io">
             <Github.Title />
             <Github.Projects />
           </Github>
@@ -56,20 +57,26 @@ class App extends Component {
           <Volunteer.Title>Volunteer</Volunteer.Title>
           <Volunteer.Items>
             <Volunteer.Item>
-              <Volunteer.Item.Company>Alma Máter UC</Volunteer.Item.Company>
-              <Volunteer.Item.Position>Founder</Volunteer.Item.Position>
-              <Volunteer.Item.Website href="https://lopezjuri.com">Website</Volunteer.Item.Website>
-              <Volunteer.Item.Period start="01-01-2016" />
+              <Volunteer.Item.Company>Censo 2017</Volunteer.Item.Company>
+              <Volunteer.Item.Position>Volunteer</Volunteer.Item.Position>
+            <Volunteer.Item.Period start="2017-4-1" />
               <Volunteer.Item.Summary>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                Sign up for voluntering.
               </Volunteer.Item.Summary>
-              <Volunteer.Item.Highlights>
-                <Volunteer.Item.Highlight>Started</Volunteer.Item.Highlight>
-                <Volunteer.Item.Highlight>Finished</Volunteer.Item.Highlight>
-              </Volunteer.Item.Highlights>
             </Volunteer.Item>
           </Volunteer.Items>
         </Volunteer>
+        <References>
+          <References.Title>References</References.Title>
+          <References.Item>
+            <References.Item.Quote>
+              This guy is a genius, before meeting him I was coding in COBOL!
+            </References.Item.Quote>
+            <References.Item.Name>
+              Nicolas Gebauer
+            </References.Item.Name>
+          </References.Item>
+        </References>
       </Resume>
     );
   }
